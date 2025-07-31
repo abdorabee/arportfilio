bc09f916291c9b50dae0e1f02545edf95c0524fd
abdorabee <abdorabee1134@gmail.com>
2025-07-31 17:15:01 +0300

    tailwindcss config

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Spotlight } from './ui/Spotlight'
 export const Hero = () => {
   return (
    <div className='pb-20 pt-36'>
-    <div></div>
+    <div className='relative'>
+      <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen fill="white"'/>
+      <Spotlight className='top-10 left-full h-[80vh] w-[50vh] fill="purple"'/>
+      <Spotlight className='top-28 left-80 h-[80vh] w-[50vh] fill="blue"'/>
+    </div>
    </div>
   )
 }
